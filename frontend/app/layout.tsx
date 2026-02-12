a0b544a069a1ed7e0de22f1eab96dc754b057d54
Reddawn <35641620+forumevi@users.noreply.github.com>
2026-02-12 20:34:11 +0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,44 +1,11 @@
-"use client"
-
-import "@rainbow-me/rainbowkit/styles.css"
-import { ReactNode } from "react"
-
-import { WagmiConfig, configureChains, createConfig } from "wagmi"
-import { base } from "wagmi/chains"
-import { publicProvider } from "wagmi/providers/public"
-
-import {
-  RainbowKitProvider,
-  getDefaultWallets
-} from "@rainbow-me/rainbowkit"
-
-const { chains, publicClient } = configureChains(
-  [base],
-  [publicProvider()]
-)
-
-const { connectors } = getDefaultWallets({
-  appName: "CastQuest",
-  projectId: "BURAYA_WALLETCONNECT_PROJECT_ID_YAPIŞTIR",
-  chains
-})
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient
-})
-
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body>
-        <WagmiConfig config={wagmiConfig}>
-          <RainbowKitProvider chains={chains}>
-            {children}
-          </RainbowKitProvider>
-        </WagmiConfig>
-      </body>
+      <body>{children}</body>
     </html>
   )
 }
