bb3ff5ce033a3a44ccc047082279144bce941d61
Reddawn <35641620+forumevi@users.noreply.github.com>
2026-02-12 20:39:38 +0300

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,11 +1,26 @@
+"use client"
+
+import { WagmiProvider } from "wagmi"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { config } from "../lib/wallet"
+import { useState } from "react"
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <WagmiProvider config={config}>
+          <QueryClientProvider client={queryClient}>
+            {children}
+          </QueryClientProvider>
+        </WagmiProvider>
+      </body>
     </html>
   )
 }
